2dfac97feff90f095a06e3f5176ecf4061a9ebe7
Tanya Bihag <tbihag@gmail.com>
2020-01-22 01:45:15 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,20 @@
 class Owner
 attr_reader :name, :species
+@@all = []
 
 def initialize(name, species)
   @name = name 
   @species = "human"
+  @@all << self
 end
 
 def say_species
+  puts "I am a #{self.species}"
+end
+
+def self.all
+  @@all
+end
+
+def self.count
   
