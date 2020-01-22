f8b3bd6c72c22fc43fea6afd9798bb5b98ebf5b2
Tanya Bihag <tbihag@gmail.com>
2020-01-22 01:31:16 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,13 @@
 class Cat
 attr_accessor :owner, :mood
 attr_reader :name
+@@all = []
 
 def initialize(name, owner)
   @name = name
   @owner = owner
   @mood = "nervous"
-  
+  @@all << self
+end
+
 end
